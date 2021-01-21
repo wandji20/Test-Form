5d6274a8625431c82ee0438f9ef643c3a32263f0
wandji20 <wbertrand10@gmail.com>
2021-01-21 20:24:50 +0100

    Add users to users relation

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,10 +3,17 @@ class UsersController < ApplicationController
   end
 
   def create
+    @user = User.new(user_params)
+    #  @user = User.new(user_name: params[:user_name], email: params[:email], password: params[:password])
+    if @user.save
+      redirect_to new_user_path
+    else
+      render :new
+    end
   end
 
   private
   def user_params
-    params.require(user).permit(:user_name, :email, :password)
+    params.require(:user).permit(:user_name, :email, :password)
   end
 end
